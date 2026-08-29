b83a6e46a65a402f0ad4377e6f19d71a142bdea9
KadirrYilmazz <ky1903190@gmail.com>
2026-08-29 17:56:53 +0300

    Kırmızı gider başlıklarında önbelleği yenile

--- pire-language.js
+++ pire-language.js
@@ -328,12 +328,12 @@
 ;(()=>{
   if(!document.querySelector('link[data-pire-finance-menu]')){
     const link=document.createElement('link');
-    link.rel='stylesheet';link.href='/pire-finance-menu.css?v=6';link.dataset.pireFinanceMenu='true';
+    link.rel='stylesheet';link.href='/pire-finance-menu.css?v=7';link.dataset.pireFinanceMenu='true';
     document.head.appendChild(link);
   }
   if(!document.querySelector('script[data-pire-finance-menu]')){
     const script=document.createElement('script');
-    script.src='/pire-finance-menu.js?v=6';script.defer=true;script.dataset.pireFinanceMenu='true';
+    script.src='/pire-finance-menu.js?v=7';script.defer=true;script.dataset.pireFinanceMenu='true';
     document.head.appendChild(script);
   }
 })();
