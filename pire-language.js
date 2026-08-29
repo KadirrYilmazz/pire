f730014558e64ad0b3996f743a570d3d4bf1c3e8
KadirrYilmazz <ky1903190@gmail.com>
2026-08-29 17:17:44 +0300

    Finans menüsünü tüm panellerde kesin yükle

--- pire-language.js
+++ pire-language.js
@@ -323,3 +323,17 @@
     document.head.appendChild(script);
   }
 })();
+
+
+;(()=>{
+  if(!document.querySelector('link[data-pire-finance-menu]')){
+    const link=document.createElement('link');
+    link.rel='stylesheet';link.href='/pire-finance-menu.css?v=2';link.dataset.pireFinanceMenu='true';
+    document.head.appendChild(link);
+  }
+  if(!document.querySelector('script[data-pire-finance-menu]')){
+    const script=document.createElement('script');
+    script.src='/pire-finance-menu.js?v=2';script.defer=true;script.dataset.pireFinanceMenu='true';
+    document.head.appendChild(script);
+  }
+})();
