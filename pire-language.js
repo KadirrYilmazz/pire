5528a58f066b5d9af62a08e73309d43375c5b723
KadirrYilmazz <ky1903190@gmail.com>
2026-08-29 17:41:42 +0300

    Yeni cari alacak sekmelerinde önbelleği yenile

--- pire-language.js
+++ pire-language.js
@@ -328,12 +328,12 @@
 ;(()=>{
   if(!document.querySelector('link[data-pire-finance-menu]')){
     const link=document.createElement('link');
-    link.rel='stylesheet';link.href='/pire-finance-menu.css?v=2';link.dataset.pireFinanceMenu='true';
+    link.rel='stylesheet';link.href='/pire-finance-menu.css?v=3';link.dataset.pireFinanceMenu='true';
     document.head.appendChild(link);
   }
   if(!document.querySelector('script[data-pire-finance-menu]')){
     const script=document.createElement('script');
-    script.src='/pire-finance-menu.js?v=2';script.defer=true;script.dataset.pireFinanceMenu='true';
+    script.src='/pire-finance-menu.js?v=3';script.defer=true;script.dataset.pireFinanceMenu='true';
     document.head.appendChild(script);
   }
 })();
